ed40e544e224d461915880f5565d84fe8d5e884d
JR Cologne <kontakt@jr-cologne.de>
2018-06-22 22:50:34 +0200

    Basic setup

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+const gulp                      = require('gulp'),
+      del                       = require('del'),
+      sourcemaps                = require('gulp-sourcemaps'),
+      plumber                   = require('gulp-plumber'),
+      sass                      = require('gulp-sass'),
+      autoprefixer              = require('gulp-autoprefixer'),
+      cssnano                   = require('gulp-cssnano'),
+      babel                     = require('gulp-babel'),
+      uglify                    = require('gulp-uglify'),
+      concat                    = require('gulp-concat'),
+      browserSync               = require('browser-sync').create(),
+
+      src_folder                = './src/',
+      src_assets_folder         = src_folder + 'assets/',
+      dist_folder               = './dist/',
+      dist_assets_folder        = dist_folder + 'assets/',
+      node_modules_folder       = './node_modules/',
+      dist_node_modules_folder  = dist_folder + 'node_modules/',
+
+      node_dependencies         = [
+        // 'package-name'
+      ];
+
+gulp.task('clear', () => del([ dist_folder ]));
+
+gulp.task('html', () => {
+  return gulp.src([ src_folder + '**/*.html' ], { base: src_folder })
+    .pipe(gulp.dest(dist_folder))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', () => {
+  return gulp.src([ src_assets_folder + 'sass/**/*.sass' ])
+    .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer({
+        browsers: [ 'last 3 versions', '> 0.5%' ]
+      }))
+      .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist_assets_folder + 'css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', () => {
+  return gulp.src([ src_assets_folder + 'js/**/*.js' ])
+    .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(babel({
+        presets: [ 'env' ]
+      }))
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist_assets_folder + 'js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('vendor', () => {
+  return gulp.src(node_dependencies.map(dependency => node_modules_folder + dependency + '/**/*.*'), { base: node_modules_folder })
+    .pipe(gulp.dest(dist_node_modules_folder))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('build', gulp.series('clear', 'html', 'sass', 'js', 'vendor'));
+
+gulp.task('serve', () => {
+  return browserSync.init({
+    server: {
+      baseDir: [ 'dist' ],
+      port: 3000
+    },
+    open: false
+  });
+});
+
+gulp.task('watch', () => {
+  let watch = [
+    src_folder + '**/*.html',
+    src_assets_folder + 'sass/**/*.sass',
+    src_assets_folder + 'js/**/*.js'
+  ];
+
+  node_dependencies.forEach(dependency => {
+    watch.push(node_modules_folder + dependency + '/**/*.*');
+  });
+
+  gulp.watch(watch, gulp.series('build')).on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')));
